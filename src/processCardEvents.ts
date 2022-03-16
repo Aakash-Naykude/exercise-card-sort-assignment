432fa48c907d54fe2e4176fcb47e6e80b6b68065
Aakash-Naykude <aakashnaykude123@gmail.com>
2022-03-16 11:06:05 +0530

    solved problem

---
+++ src/processCardEvents.ts
@@ -0,0 +1,43 @@
+import { CardEvent, Transaction } from './types'
+
+type CardTransactionMapping = {
+  [cardId: string]: Transaction
+}
+
+/**
+ * Write a function that receives a large batch of card events from multiple cards,
+ * returning an object which maps from cardId -> valid transaction. Only cardIds with
+ * a valid transaction should appear in the returned object.
+ *
+ * A valid transaction is a pair of card events, starting with a RESERVATION event
+ * and finishing with either a CONFIRMATION or CANCELLATION event.
+ *
+ * The input is an array of unprocessed card events. Some events might be duplicated
+ * or missing. For duplicated events, you may only use one of its occurrences and
+ * discard the rest. Missing events invalidate the transaction.
+ *
+ * @param cardEvents CardEvent[] List of card events
+ * @returns CardTransactionMapping Valid transactions grouped by cardId
+ */
+export const processCardEvents = (cardEvents: CardEvent[]): CardTransactionMapping => {
+
+  cardEvents = cardEvents.sort()
+
+  let object: { [CardId: string]: CardEvent[] } = {}
+
+  for (var i = 0; i < cardEvents.length-1 ; i++) {
+    if (
+      cardEvents[i].cardId == cardEvents[i + 1].cardId &&
+      ((cardEvents[i].type == 'RESERVATION' && cardEvents[i + 1].type == 'CANCELLATION') ||
+        (cardEvents[i].type == 'RESERVATION' && cardEvents[i + 1].type == 'CONFIRMATION'))
+    ) {
+      var out: CardEvent[] = []
+      out.push(cardEvents[i])
+      out.push(cardEvents[i + 1])
+      object[cardEvents[i].cardId] = out
+    }
+  }
+
+
+  return object as CardTransactionMapping
+}
